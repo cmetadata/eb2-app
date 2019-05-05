9fde648363c56286d402e9a5e34dca13b592f4b4
cmetadata <42916040+cmetadata@users.noreply.github.com>
2019-05-04 20:25:20 -0400

    Update and rename User.php to Version.php

---
+++ app/Version.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Version extends Model
+{
+    protected $table = 'versions';
+    protected $primaryKey = 'version';
+    
+}
